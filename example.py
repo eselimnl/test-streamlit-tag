f758ae67e13d09da8191a71c7f02e619b96933cd
eselimnl <eselimnl@gmail.com>
2024-05-12 16:09:23 +0100

    cookie banner

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 
 from streamlit_gtag import st_gtag
 
@@ -8,17 +9,45 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("Google Analytics :chart:")
+# Activate session state
+
+# Initialize session state
+if 'cookie' not in st.session_state:
+    st.session_state['cookie'] = 'false'
 
 
-if st.button("Send Event A"):
-    st_gtag(
-        key="gtag_send_event_b",
-        id="G-8H46WZXW14",
-        event_name="send_event_button",
-        params={
-            "event_category": "test_category_b",
-            "event_label": "test_label_b",
-            "value": 97,
-        },
-    )
+def cookie_banner():
+    if st.session_state['cookie'] == 'false':
+        placeholder = st.empty()
+
+        # Define the cookie banner layout
+        col0, col1, col2, col3, col4 = placeholder.columns([5, 2.5, 0.5, 0.5, 5])
+
+        # Display cookie message in the first column
+        col1.write("We use cookies to improve your experience on our website.")
+
+        # If "Accept" button is clicked, set session state to 'true'
+        if col2.button("Accept", type='primary'):
+            st.session_state['cookie'] = 'true'
+            st_gtag(
+                key="gtag_send_event_b",
+                id="G-8H46WZXW14",
+                event_name="send_event_button",
+                params={
+                    "event_category": "test_category_b",
+                    "event_label": "test_label_b",
+                    "value": 97,
+                }
+            )
+
+        # If "Reject" button is clicked, set session state to 'true'
+        if col3.button("Reject"):
+            st.session_state['cookie'] = 'true'
+
+        # If either "Accept" or "Reject" button is clicked, remove the cookie banner
+        if st.session_state['cookie'] == 'true':
+            placeholder.empty()
+
+
+cookie_banner()
+st.title("Google Analytics:chart:")
